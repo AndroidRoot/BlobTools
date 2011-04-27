5dc2432cb6a72c414c08dd9ec40a5d5b421c5672
RaYmAn <rayman@androidroot.mobi>
2011-04-27 20:56:56 +0200

    Fixed indentation etc and updated blobunpack significantly. Now stores to . and stores full header in .HEADER

--- src/blobpack.c
+++ src/blobpack.c
@@ -4,63 +4,65 @@
 
 #include "blob.h"
 
-int main(int argc, char **argv)
+int
+main (int argc, char **argv)
 {
-header_type hdr;
-memset(&hdr, 0, sizeof(header_type));
+  header_type hdr;
+  memset (&hdr, 0, sizeof (header_type));
 
-if(argc < 3)
-{
-  printf("Usage: %s <blobname> <outfile>\n", argv[0]);
+  if (argc < 3)
+    {
+      printf ("Usage: %s <blobname> <outfile>\n", argv[0]);
+      return 0;
+    }
+  char *blobname = argv[1];
+  char *outname = argv[2];
+
+  FILE *fil = fopen (blobname, "r");
+  FILE *outfile = fopen (outname, "w");
+
+  fread (&hdr, sizeof (header_type), 1, fil);
+  fwrite (&hdr, sizeof (header_type), 1, outfile);
+  if (!memcmp (hdr.magic, MAGIC, MAGIC_SIZE))
+    {
+      printf ("Correct header\n");
+    }
+  printf ("Size: %d\n", hdr.size);
+  printf ("%d partitions starting at offset 0x%X\n", hdr.num_parts,
+	  hdr.part_offset);
+  if (sizeof (header_type) != hdr.part_offset)	// If the partitions don't start exactly after the header, it's an unknown type possibly?
+    {
+      printf ("Error?");
+      return 0;
+    }
+
+  fseek (fil, hdr.part_offset, SEEK_SET);
+  part_type *parts = calloc (hdr.num_parts, sizeof (part_type));
+  fread (parts, sizeof (part_type), hdr.num_parts, fil);
+  int i;
+  int currentOffset = parts[0].offset;
+  for (i = 0; i < hdr.num_parts; i++)
+    {
+      printf ("Partition %d\n", i);
+      printf ("Name: %s\n", parts[i].name);
+      parts[i].offset = currentOffset;	// Update offset  in case previous files have changed size
+      FILE *curfile = fopen (parts[i].name, "r");
+      fseek (curfile, 0, SEEK_END);
+      long fsize = ftell (curfile);
+      fclose (curfile);
+      parts[i].size = fsize;	// Update size
+      currentOffset += fsize;
+    }
+  fwrite (parts, sizeof (part_type), hdr.num_parts, outfile);
+  for (i = 0; i < hdr.num_parts; i++)
+    {
+      char *buffer = (char *) malloc (parts[i].size);
+      FILE *currFile = fopen (parts[i].name, "r");	// Read in update file
+      fread (buffer, 1, parts[i].size, currFile);
+      fwrite (buffer, 1, parts[i].size, outfile);
+    };
+
+  fclose (fil);
+  fclose (outfile);
   return 0;
-}
-char *blobname = argv[1];
-char *outname = argv[2];
-
-FILE *fil = fopen(blobname, "r");
-FILE *outfile = fopen(outname, "w");
-
-fread(&hdr, sizeof(header_type),  1, fil);
-fwrite(&hdr, sizeof(header_type), 1, outfile);
-if(!memcmp(hdr.magic, MAGIC, MAGIC_SIZE))
-{
-  printf("Correct header\n");
-}
-printf("Size: %d\n", hdr.size);
-printf("%d partitions starting at offset 0x%X\n", hdr.num_parts, hdr.part_offset);
-if(sizeof(header_type) != hdr.part_offset) // If the partitions don't start exactly after the header, it's an unknown type possibly?
-{
-  printf("Error?");
-  return 0;
-}
-
-fseek(fil, hdr.part_offset, SEEK_SET);
-part_type *parts = calloc(hdr.num_parts, sizeof(part_type));
-fread(parts, sizeof(part_type), hdr.num_parts, fil);
-int i;
-int currentOffset=parts[0].offset;
-for(i=0;i<hdr.num_parts; i++)
-{
-  printf("Partition %d\n",i);
-  printf("Name: %s\n", parts[i].name);
-  parts[i].offset = currentOffset; // Update offset  in case previous files have changed size
-  FILE* curfile = fopen(parts[i].name, "r");
-  fseek(curfile, 0, SEEK_END);
-  long fsize = ftell(curfile);
-  fclose(curfile);
-  parts[i].size = fsize; // Update size
-  currentOffset += fsize;
-}
-fwrite(parts, sizeof(part_type), hdr.num_parts, outfile);
-for(i=0;i<hdr.num_parts; i++)
-{
-  char *buffer = (char*)malloc(parts[i].size);
-  FILE *currFile = fopen(parts[i].name, "r"); // Read in update file
-  fread(buffer, 1, parts[i].size, currFile);
-  fwrite(buffer, 1, parts[i].size, outfile);
-};
-
-fclose(fil);
-fclose(outfile);
-return 0;
 }
--- src/blobunpack.c
+++ src/blobunpack.c
@@ -3,58 +3,78 @@
 #include <string.h>
 #include "blob.h"
 
-void dumpPartition(FILE *fil, part_type part);
-
-int main(int argc, char **argv)
-{
-header_type hdr;
-memset(&hdr, 0, sizeof(header_type));
-
-if(argc < 2)
-  return 0;
-
-FILE *fil = fopen(argv[1], "r");
-fread(&hdr, sizeof(header_type),  1, fil);
-if(!memcmp(hdr.magic, MAGIC, MAGIC_SIZE))
-{
-  printf("Correct header\n");
-}
-printf("Size: %d\n", hdr.size);
-printf("%d partitions starting at offset 0x%X\n", hdr.num_parts, hdr.part_offset);
-
-fseek(fil, hdr.part_offset, SEEK_SET);
-part_type *parts = calloc(hdr.num_parts, sizeof(part_type));
-fread(parts, sizeof(part_type), hdr.num_parts, fil);
-int i;
-for(i=0;i<hdr.num_parts; i++)
-{
-  printf("Partition %d\n",i);
-  printf("Name: %s\n", parts[i].name);
-  printf("ImageOffset: %d (0x%X)\n", parts[i].offset, parts[i].size);
-  printf("ImageLength: %d (0x%X)\n", parts[i].size, parts[i].size);
-  dumpPartition(fil, parts[i]);
-}
-fclose(fil);
-return 0;
-}
-
+void dumpPartition (FILE * file, char *basename, part_type part);
 #define BUFFER_SIZE 2048
-void dumpPartition(FILE *fil, part_type part)
+
+int
+main (int argc, char **argv)
 {
-fseek(fil, part.offset, SEEK_SET);
-int dataleft = part.size;
-char buffer[BUFFER_SIZE];
-printf("Writing file %s\n", part.name);
-printf("FileSize: %d bytes\n", part.size);
-FILE* outfile = fopen(part.name, "w");
-while(dataleft > 0)
-{
-  int toRead = dataleft > BUFFER_SIZE ? BUFFER_SIZE : dataleft;
-  int dataread = fread(buffer, 1,toRead, fil);
-  int datawritten = fwrite(buffer, 1,dataread, outfile);
-  if(dataread != datawritten)
-   abort();
-  dataleft -= dataread;
+  header_type hdr;
+  memset (&hdr, 0, sizeof (header_type));
+
+  if (argc < 2)
+    {
+      fprintf (stderr, "Usage: %s <blobfile>\n", argv[0]);
+      return -1;
+    }
+
+  FILE *file = fopen (argv[1], "r");
+  if (file == NULL)
+    {
+      fprintf (stderr, "Unable to open \"%s\"\n", argv[1]);
+      return -1;
+    }
+  fread (&hdr, sizeof (header_type), 1, file);
+  if (memcmp (hdr.magic, MAGIC, MAGIC_SIZE))
+    {
+      fprintf (stderr, "File \"%s\" is not a valid blob file\n", argv[1]);
+      return -1;
+    }
+  printf ("Header size: %d\n", hdr.size);
+  printf ("%d partitions starting at offset 0x%X\n", hdr.num_parts,
+	  hdr.part_offset);
+
+  char hdrfilename[1024];
+  snprintf (hdrfilename, 1024, "%s.HEADER", argv[1]);
+  FILE *hdrfile = fopen (hdrfilename, "w");
+  fwrite (&hdr, sizeof (header_type), 1, hdrfile);
+  fclose (hdrfile);
+
+
+  fseek (file, hdr.part_offset, SEEK_SET);
+  part_type *parts = calloc (hdr.num_parts, sizeof (part_type));
+  fread (parts, sizeof (part_type), hdr.num_parts, file);
+  int i;
+  for (i = 0; i < hdr.num_parts; i++)
+    {
+      printf ("Partition %d\n", i);
+      printf ("Name: %s\n", parts[i].name);
+      printf ("Offset: %d (0x%X)\n", parts[i].offset, parts[i].offset);
+      printf ("Size: %d (0x%X)\n", parts[i].size, parts[i].size);
+      dumpPartition (file, argv[1], parts[i]);
+    }
+  fclose (file);
+  return 0;
 }
-fclose(outfile);
+
+void
+dumpPartition (FILE * file, char *basename, part_type part)
+{
+  fseek (file, part.offset, SEEK_SET);
+  int dataleft = part.size;
+  char buffer[BUFFER_SIZE];
+  char filename[1024];
+  snprintf (filename, 1024, "%s.%s", basename, part.name);
+  printf ("Writing file %s (%d bytes)\n", filename, part.size);
+  FILE *outfile = fopen (filename, "w");
+  while (dataleft > 0)
+    {
+      int toRead = dataleft > BUFFER_SIZE ? BUFFER_SIZE : dataleft;
+      int dataread = fread (buffer, 1, toRead, file);
+      int datawritten = fwrite (buffer, 1, dataread, outfile);
+      if (dataread != datawritten)
+	abort ();
+      dataleft -= dataread;
+    }
+  fclose (outfile);
 }
