612da1f2de35f364f956f5529c529600e50708df
drwowe <drwowe@yahoo.com>
2012-02-10 12:05:30 -0500

    support signed blob as introduced by Transformer Prime     culled from https://github.com/sparkym3/BlobTools

--- shared/blob.h
+++ shared/blob.h
@@ -4,12 +4,15 @@
 // Previous unknown fields grabbed from 
 // http://nv-tegra.nvidia.com/gitweb/?p=android/platform/build.git;a=commitdiff;h=0adc4478615891636d8b7c476c20c2014b788537
 
+#define SIGNBLOB "\x2d\x53\x49\x47\x4e\x45\x44\x2d\x42\x59\x2d\x53\x49\x47\x4e\x42\x4c\x4f\x42\x2d\x29\xc3\x94\x20\x00\x01\x00\x00"	//"-SIGNED-BY-SIGNBLOB-)Ô "
+#define SIGNBLOB_SIZE 28
 #define MAGIC       "MSM-RADIO-UPDATE"
 #define MAGIC_SIZE  16
 #define PART_NAME_LEN 4
 
 typedef struct
 {
+    unsigned char signblob[SIGNBLOB_SIZE];
     unsigned char magic[MAGIC_SIZE];
     unsigned int version; // Always 0x00010000
     unsigned int size; // Size of header
